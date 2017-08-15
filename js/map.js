683bcad51d7c7e609dca80f614b2de86d6509abc
Marcello Balduccini <marcello.balduccini@drexel.edu>
2017-08-15 16:55:53 -0400

    Enabling multiple markers on map.

---
+++ js/map.js
@@ -0,0 +1,75 @@
+/* from https://wrightshq.com/playground/placing-multiple-markers-on-a-google-map-using-api-3/ */
+
+jQuery(function($) {
+    // Asynchronously Load the map API 
+    var script = document.createElement('script');
+    script.src = "//maps.googleapis.com/maps/api/js?sensor=false&callback=initialize&key=AIzaSyB9_SZIXQgUw4VWA4GdCDpZwKT1o-HtwUA";
+    document.body.appendChild(script);
+});
+
+function initialize() {
+    var map;
+    var bounds = new google.maps.LatLngBounds();
+    var mapOptions = {
+        mapTypeId: 'roadmap'
+    };
+                    
+    // Display a map on the page
+    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    map.setTilt(45);
+        
+    // Multiple Markers
+    var markers = [
+//        ['London Eye, London', 51.503454,-0.119562],
+//        ['Palace of Westminster, London', 51.499633,-0.124755]
+        ['Drexel University', 39.9566127,-75.1921381],
+        ['Saint Joseph\'s University', 39.9946873,-75.2438292]
+    ];
+                        
+    // Info Window Content
+    var infoWindowContent = [
+//        ['<div class="info_content">' +
+//        '<h3>London Eye</h3>' +
+//        '<p>The London Eye is a giant Ferris wheel situated on the banks of the River Thames. The entire structure is 135 metres (443 ft) tall and the wheel has a diameter of 120 metres (394 ft).</p>' +        '</div>'],
+//        ['<div class="info_content">' +
+//        '<h3>Palace of Westminster</h3>' +
+//        '<p>The Palace of Westminster is the meeting place of the House of Commons and the House of Lords, the two houses of the Parliament of the United Kingdom. Commonly known as the Houses of Parliament after its tenants.</p>' +
+//        '</div>']
+        ['<div class="info_content">' +
+        '<h3>Drexel University</h3><p>3141 Chestnut St, Philadelphia, PA 19104</p></div>'],
+        ['<div class="info_content">' +
+        '<h3>Saint Joseph\'s University</h3><p>5600 City Ave, Philadelphia, PA 19131</p></div>']
+    ];
+        
+    // Display multiple markers on a map
+    var infoWindow = new google.maps.InfoWindow(), marker, i;
+    
+    // Loop through our array of markers & place each one on the map  
+    for( i = 0; i < markers.length; i++ ) {
+        var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+        bounds.extend(position);
+        marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: markers[i][0]
+        });
+        
+        // Allow each marker to have an info window    
+//        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+//            return function() {
+//                infoWindow.setContent(infoWindowContent[i][0]);
+//                infoWindow.open(map, marker);
+//            }
+//        })(marker, i));
+
+        // Automatically center the map fitting all markers on the screen
+        map.fitBounds(bounds);
+    }
+
+    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+//    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+//        this.setZoom(13);
+//        google.maps.event.removeListener(boundsListener);
+//    });
+    
+}
